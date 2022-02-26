6e33293e6f8085878505d740901a901f0164b014
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-02-26 15:40:57 +0000

    update cors

--- app.py
+++ app.py
@@ -21,27 +21,23 @@ results = [dict(row) for row in test]
 
 
 @app.route('/')
-@cross_origin()
 def index():
     return jsonify(results)
 
 # Send back a random book.
 # Get an array from the client of which index's have already been used.
 @app.route('/random')
-@cross_origin()
 def random_book():
     book_length = len(results)
     random_index = random.randint(0, book_length -1)
     return jsonify(results[random_index])
 
 @app.route('/shuffle')
-@cross_origin()
 def shuffled_books():
     random.shuffle(results)
     return jsonify(results)
 
 @app.route('/play/<int:number_of_books>')
-@cross_origin()
 def get_game_books(number_of_books):
     if number_of_books > len(results):
         return
